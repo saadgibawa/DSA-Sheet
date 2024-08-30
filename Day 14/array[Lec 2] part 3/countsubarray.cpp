324f52319eb48072bae71b6371478190194c7ba1
Saadgi Bawa <saadgibawa31@gmail.com>
2024-08-30 23:00:20 +0530

    Array : medium

---
+++ Day 14/array[Lec 2] part 3/countsubarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size(); 
+        unordered_map<int, int> mpp;
+        int preSum = 0, cnt = 0;
+        mpp[0] = 1;
+        for (int i = 0; i < n; i++) {
+            preSum += nums[i];
+            int remove = preSum - k;
+            cnt += mpp[remove];
+            mpp[preSum] += 1;
+        }
+        return cnt;
+    }
+};
