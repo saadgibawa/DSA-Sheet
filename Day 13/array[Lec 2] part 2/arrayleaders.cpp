b212bfe6bd0059bd7b605330f56f889ce726195e
Saadgi Bawa <saadgibawa31@gmail.com>
2024-08-30 22:25:43 +0530

    Add files via upload

---
+++ Day 13/array[Lec 2] part 2/arrayleaders.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    // Function to find the leaders in the array.
+  public:
+    vector<int> leaders(int n, int arr[]) {
+        // Code here
+        vector<int>Leaders;
+        Leaders.push_back(arr[n-1]);
+        int start=arr[n-1];
+        for(int i=n-1;i>=0;i--){
+            if(arr[i-1]>=start){
+                Leaders.insert(Leaders.begin(), arr[i-1]);
+                start=arr[i-1];
+            }
+        }
+        return Leaders;
+    }
+};
