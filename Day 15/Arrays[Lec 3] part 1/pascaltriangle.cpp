64ae3366f02dfb947d9dd589ee1fc8c81fde109e
Saadgi Bawa <saadgibawa31@gmail.com>
2024-08-31 17:27:35 +0530

    Array : hard problems[1]

---
+++ Day 15/Arrays[Lec 3] part 1/pascaltriangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> fun(int n){
+      vector<int> v;
+      v.push_back(1);
+      int res=1;
+      for(int i=1;i<n;i++){
+          res=res*(n-i);
+          res=res/i;
+          v.push_back(res);
+      }  
+      return v;
+    }
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans;
+        for(int i=0;i<numRows;i++){
+            vector<int> temp;
+            temp=fun(i+1);
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
