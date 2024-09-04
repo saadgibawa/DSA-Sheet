d83c681b76cc9d01a2058ba54bb445c4631012bb
Saadgi Bawa <saadgibawa31@gmail.com>
2024-09-04 14:46:33 +0530

    basics : basic hashing

--- Day 7/Basics[Lec 6]/countingfrequency.cpp
+++ Day 7/Basics[Lec 6]/countingfrequency.cpp
@@ -1,8 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-// } Driver Code Ends
 class Solution {
   public:
     // Function to count the frequency of all elements from 1 to N in the array.
@@ -22,39 +17,3 @@ class Solution {
         
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    long long t;
-
-    // testcases
-    cin >> t;
-
-    while (t--) {
-
-        int N, P;
-        // size of array
-        cin >> N;
-
-        vector<int> arr(N);
-
-        // adding elements to the vector
-        for (int i = 0; i < N; i++) {
-            cin >> arr[i];
-        }
-        cin >> P;
-        Solution ob;
-        // calling frequncycount() function
-        ob.frequencyCount(arr, N, P);
-
-        // printing array elements
-        for (int i = 0; i < N; i++)
-            cout << arr[i] << " ";
-        cout << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
