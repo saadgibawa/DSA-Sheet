c198efa4e61766a5004f88e0441a630d27386a24
Saadgi Bawa <saadgibawa31@gmail.com>
2024-09-04 14:55:51 +0530

    s

--- Day 9/Sorting[Lec 2]/mergesort.cpp
+++ Day 9/Sorting[Lec 2]/mergesort.cpp
@@ -1,21 +1,3 @@
-//{ Driver Code Starts
-#include <stdio.h>
-#include <bits/stdc++.h>
-using namespace std;
-
-
-
-/* Function to print an array */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-
-// } Driver Code Ends
 class Solution
 {
     public:
@@ -62,27 +44,3 @@ class Solution
         merge(arr, l, m, r);
     }
 };
-
-//{ Driver Code Starts.
-
-
-int main()
-{
-    int n,T,i;
-
-    scanf("%d",&T);
-
-    while(T--){
-    
-    scanf("%d",&n);
-    int arr[n+1];
-    for(i=0;i<n;i++)
-      scanf("%d",&arr[i]);
-
-    Solution ob;
-    ob.mergeSort(arr, 0, n-1);
-    printArray(arr, n);
-    }
-    return 0;
-}
-// } Driver Code Ends
