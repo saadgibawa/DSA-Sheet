695bd91e235931ffa7b7e729b67ab4a20420dea5
Saadgi Bawa <saadgibawa31@gmail.com>
2024-09-04 15:07:12 +0530

    Array : Medium problems[1]

--- Day 12/array[Lec 2] part 1/sortarray.cpp
+++ Day 12/array[Lec 2] part 1/sortarray.cpp
@@ -5,15 +5,18 @@ public:
         int low = 0, mid = 0, high = n - 1; 
 
         while (mid <= high) {
-            if (arr[mid] == 0) {
+            if (arr[mid] == 0) 
+            {
                 swap(arr[low], arr[mid]);
                 low++;
                 mid++;
             }
-            else if (arr[mid] == 1) {
+            else if (arr[mid] == 1) 
+            {
                 mid++;
             }
-            else {
+            else 
+            {
                 swap(arr[mid], arr[high]);
                 high--;
             }
