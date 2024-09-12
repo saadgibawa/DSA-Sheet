431586f435d807bb78f69f5d89f12c0c91194860
Saadgi Bawa <saadgibawa31@gmail.com>
2024-09-12 23:58:55 +0530

    Binary Search : BS on 1D arrays[1]

--- Day 19/Binary search[Lec 1] part 1/lowerbound.cpp
+++ Day 19/Binary search[Lec 1] part 1/lowerbound.cpp
@@ -1,9 +1,7 @@
 
 class Solution {
   public:
-    // Function to find floor of x
-    // n: size of vector
-    // x: element whose floor is to find
+    
     int findFloor(vector<long long> &v, long long n, long long x) {
         int low = 0, high = n - 1;
         int floorIndex = -1;
